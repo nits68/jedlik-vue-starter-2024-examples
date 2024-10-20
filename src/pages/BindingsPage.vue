eec070c48ef02e13745b25785d764c3f6f289d11
nits68 <nits.laszlo@jedlik.eu>
2024-10-20 22:10:45 +0200

    correct bindings page

--- src/pages/BindingsPage.vue
+++ src/pages/BindingsPage.vue
@@ -5,6 +5,8 @@
 import { ref, reactive } from 'vue';
 
 const dynamicId = ref(undefined);
+// const dynamicId = ref('myId');
+
 const id = ref('myDinamicId');
 
 const activeColor = ref('red');
@@ -12,9 +14,9 @@ const activeColor = ref('red');
 const styleObject = reactive({
   color: 'blue',
   fontSize: '80px',
-  backgroundColor: 'yellow', // CSS tulajdonság camelCase-ben is megadható snake-case helyett
+  backgroundColor: 'pink', // CSS tulajdonság camelCase-ben is megadható snake-case helyett
   // vagy:
-  // 'background-color': 'yellow',
+  // 'background-color': 'pink',
   //  background-color helyett: backgroundColor
 });
 
@@ -46,30 +48,40 @@ function toggleActive() {
 </script>
 
 <template>
-  <!-- Bindings: Egyirányú adatkötés -->
-  <!-- Vue.js direktívája a "v-bind:", rövidítése ":" -->
+  <!-- Bindings: Adatkötés -->
+  <!-- Az egyirányú adatkötés Vue.js direktívája a "v-bind:", rövidítése ":" -->
   <q-page>
     <!-- Attribute Bindings -->
-    <!-- If dynamicId value is undefined, then the attribute will be removed from the rendered element. -->
+    <!-- Ha a dynamicId értéke undefined, akkor az "id" attributum nem kerül rendelerésre-->
     <p :id="dynamicId">Bekezdés dinamikus id-val (F12-vel keresd meg az azonosítóját!)</p>
+
+    <!-- A v-model Vue.js direktíva a kétirányú adatkötésre szolgál, lsd. később -->
+    <!-- Az indításkor undefined értékű "dynamicId" változónak adunk itt értéket -->
     <q-input v-model="dynamicId" clearable label="Add meg a paragrafus azonosítóját!" outlined />
 
     <!-- Same-name Shorthand -->
-    <!-- Ha a TS/JS változó azonosítója eggyezik az attribútum azonosítójával, akkor röviden: -->
+    <!-- Ha a TS/JS változó azonosítója eggyezik a "kötendő" attribútum azonosítójával, akkor röviden: -->
     <p :id>Ennek a paragrafusnak az id-ja: 'myDinamicId'</p>
 
     <!-- Style bindings -->
-    <!-- <h2 v-bind:style="{ color: activeColor }">Style binding</h2> -->
-    <h2 :style="{ color: activeColor }">Style binding</h2>
+    <!-- Egy vagy több dinmikus CSS tulajdonságot (itt color és background-color) objektummal határozzuk meg -->
+    <!-- Ha kötőjel van a tulajdonság nevében, akkor camelCase-ben kell megadni a tulajdonságot, -->
+    <!-- vagy a tulajdonság azonosítóját (background-color) aposztrófok közé kell tenni. -->
+    <h2 :style="{ color: activeColor, 'background-color': 'yellow' }">Style binding</h2>  
     <q-input v-model="activeColor" clearable label="Szín" outlined />
+       
+    <!-- Több dinamikus CSS tulajdonság megadásakor, célszerű objektumot (styleObject) készíteni a script blokkban: -->
     <h2 :style="styleObject">Style bindings with object</h2>
 
     <!-- Class bindings -->
+    <!-- A 'bold' class hozzárendelését az isBold logikai változó értéke vezérli -->
+    <!-- Igaz esetén hozzárendeli a CSS osztályt, hamis esetén nem  -->
     <h2 :class="{ bold: isBold }">Class binding - is bold?</h2>
     <q-btn color="primary" glossy label="Bold/Normal" no-caps @click="toggleBold()" />
 
-    <!-- Példa attributes binding-ra Quasar elemekkel, boolean típusú attribútummal -->
+    <!-- Példa attribútum binding-ra Quasar elemekkel, boolean típusú attribútummal -->
     <!-- A példában az előző feladat isBold reaktív változóját használjuk -->
+    <!-- A disable és dense attribútumok beállítását (True) az isBold változó vezérli: -->
     <q-btn color="primary" :disable="isBold" glossy label="disable true/false" no-caps />
     <q-banner :dense="isBold">Ez a banner szövege (dense true/false)</q-banner>
 
@@ -78,6 +90,10 @@ function toggleActive() {
     <q-banner v-bind="objectOfAttrs">Object of attributes binding</q-banner>
 
     <!-- Péda dinamikus attributumokra -->
+    <!-- Előző példákban a CSS attribútumok értékei voltak csak dinamikusak -->
+    <!-- Azonban lehetőség van magát az attributumot is dinamikusan megadni: -->
+    <!-- A példában a no-caps és rounded attribútumokat váltogatjuk -->
+    <!-- Értéküket az isActive reaktív változó határozza meg: -->
     <q-btn :[attribNeve]="isActive">Péda dinamikus attribútumra</q-btn>
     <div>
       <q-btn
